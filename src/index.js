def74d9cae493ceb3bf9dbf8df2dfa0b03dfdc6f
Manzur Ahammed <manzur.ahammed236@gmail.com>
2020-04-19 20:48:04 +0600

    get data from api

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/**
+ * this function created for generated name and print them
+ * @param a
+ * @param b
+ */
+const generate = (a,b) => {
+    console.log(a,b);
+}
+
+/**
+ * Generated function use and create data
+ */
+const bindp = () => {
+    generate(12,34);
+}
